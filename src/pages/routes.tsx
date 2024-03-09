f354e19a9b8b2fb135101b3a928a45b6d3d7c078
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-09 19:20:30 -0300

    fix: * on routes

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -6,7 +6,7 @@ export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
