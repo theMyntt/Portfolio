a4642c6a3c1d15744b1872ad9f063d6ebe1c5314
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-09 13:14:52 -0300

    feat: router & home archive

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Router from '@pages/routes'
 
 import './main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    
+    <Router />
   </React.StrictMode>,
 )
