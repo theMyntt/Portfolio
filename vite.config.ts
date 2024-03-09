3bc3965f14a9761023d352c425ac49b842e748e0
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-09 13:19:38 -0300

    fix: path resolve

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,9 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src/"),
+      "@pages": path.resolve(__dirname, "./src/pages/"),
+      "@components": path.resolve(__dirname, "./src/components/"),
     },
   },
 })
