dda565903f24be2121e2b4106abaf2cdac7b7f33
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-09 13:53:55 -0300

    feat: header component

---
+++ src/components/Header/header.tsx
@@ -0,0 +1,29 @@
+import "./header.scss"
+
+interface Props {
+  menuItems: string[],
+  hrefItems: string[]
+}
+
+export default function Header(props: Props): JSX.Element {
+  return (
+    <div className="app-container">
+      <div className="header-container">
+        <div className="header-logo">
+          <a href="/">
+            <h2>Portfólio</h2> 
+          </a>
+        </div>
+        <div className="header-menu">
+          <ul>
+            {props.menuItems.map((item: string, index: number) => (
+              <li key={index}>
+                <a href={props.hrefItems[index]}>{item}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
