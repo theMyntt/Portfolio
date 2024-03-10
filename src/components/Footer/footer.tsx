87e965be43c856c0cf7504972ac788e1b2dd8429
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-09 21:46:34 -0300

    fix: footer component style on desktop

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -2,7 +2,7 @@ import "./footer.scss"
 
 export default function Footer(): JSX.Element {
   return (
-    <div className="footer container">
+    <div className="footer">
       <p>2024 - &copy; Todos os direitos reservados a Gabriel Araújo Lima</p>
     </div>
   )
