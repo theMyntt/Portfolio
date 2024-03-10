53cb283b764521dc82f47ad0d369e31e46408a3f
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-09 21:47:19 -0300

    feat: projects page

---
+++ src/pages/Project/project.tsx
@@ -0,0 +1,50 @@
+import Header from "@/components/Header/header";
+
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+
+import "./scss/project.scss";
+import { ArrowLeft } from "lucide-react";
+
+export default function Project(): JSX.Element {
+  interface projectsObject {
+    Nome: string;
+    Repositorio: React.ReactElement;
+    Deploy: React.ReactElement | "N/A";
+    // header: string;
+  }
+
+  const projects: projectsObject[] = [
+    { Nome: "Portfólio", Repositorio: <a href="https://github.com/themyntt/portfolio">github.com/portfólio</a>, Deploy: "N/A"},
+    { Nome: "basicT", Repositorio: <a href="https://github.com/themyntt/basicT">github.com/basicT</a>,Deploy: <a href="https://github.com/theMyntt/basicT/releases/">github.com/releases</a>},
+    { Nome: "Luxure", Repositorio: <a href="https://github.com/themyntt/luxure">github.com/Luxure</a>,Deploy: "N/A"},
+    { Nome: "Hub Eyes", Repositorio: <a href="https://github.com/themyntt/hub-eyes">github.com/HubEyes</a>,Deploy: "N/A"},
+    { Nome: "Manage++", Repositorio: <a href="https://github.com/themyntt/ManagePP">github.com/Manage++</a>,Deploy: "N/A"},
+    { Nome: "SGA", Repositorio: <a href="https://github.com/themyntt/SGA">github.com/SGA</a>,Deploy: "N/A"},
+  ];
+
+  const columns: string[] = [
+    "Nome",
+    "Repositorio",
+    "Deploy"
+  ]
+
+  return (
+    <div className="projects">
+      <Header
+        menuItems={["Inicio", "Sobre", "Projetos", "Contato"]}
+        hrefItems={["/", "./#about", "project", "./#contact"]}
+      />
+      <div className="container">
+        <h1>Meus projetos</h1> <br />
+        <DataTable value={projects} tableStyle={{ minWidth: '25rem' }}>
+          {columns.map((item: string, index: number) => (
+            <Column key={index} field={item} header={item}></Column>
+          ))}
+        </DataTable>
+        <br />
+        <button style={{display: "flex", alignItems: "center", justifyContent: "center"}} onClick={() => window.location.href = "/"}><ArrowLeft /> Voltar</button>
+      </div>
+    </div>
+  );
+}
--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "@pages/Home/home";
+import Project from "@pages/Project/project";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<Home />} />
+        <Route path="/project" element={<Project />} />
       </Routes>
     </BrowserRouter>
   );
